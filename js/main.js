7b53326d6e68cb66f5ca69d50c8ba718adc2c961
Limirida <bloodsky2312@gmail.com>
2020-04-15 07:01:21 +0300

    add adaptation for index.html

--- js/main.js
+++ js/main.js
@@ -1,53 +1,59 @@
 $('.slider-carousel').slick({
     dots: true,
+    autoplay: true,
+    prevArrow: '<img class="slick-next" src="img/main/prevArrow.svg" alt=""></img>',
+    nextArrow: '<img class="slick-prev " src="img/main/nextArrow.svg" alt=""></img>',
+    responsive: [{
+        breakpoint: 850,
+        settings: {
+            dots: false,
+            arrows: false,
+        }
+    }, ]
 });
 
 $('.import').slick({
-    arrows: true,
+    autoplay: true,
+    prevArrow: '<img class="slick-next" src="img/main/prevArrow.svg" alt=""></img>',
+    nextArrow: '<img class="slick-prev " src="img/main/nextArrow.svg" alt=""></img>',
+    responsive: [{
+        breakpoint: 850,
+        settings: {
+            arrows: false,
+        }
+    }, ]
 });
 
-/*$('.surf-slider').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    prevArrow: '<img class="slider-arrows sleder-arrows__left" src="img/arrows-left.svg" alt=""></img>',
-    nextArrow: '<img class="slider-arrows " src="img/arrows-right.svg" alt=""></img>',
-    asNavFor: '.slider-map',
-    responsive: [{
-            breakpoint: 1210,
-            settings: {
-                slidesToShow: 3,
-            }
-        },
-        {
-            breakpoint: 900,
-            settings: {
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 720,
-            settings: {
-                slidesToShow: 1,
-                centerMode: true,
-            }
-        },
-        {
-            breakpoint: 426,
-            settings: {
-                slidesToShow: 1,
-                centerMode: false,
-            }
-        },
-    ]
-});
-*/
 
 
 $(document).ready(function () {
-    $('.header-btn').on("click", function () {
+    $('.popup-application--open').on("click", function () {
         $('.overlay').show();
     });
     $('.popup-close').on("click", function () {
         $('.overlay').hide();
+    });
+});
+
+$(document).ready(function () {
+    $('.popup-call--open').on("click", function () {
+        $('.overlay--call').show();
+    });
+    $('.popup-close').on("click", function () {
+        $('.overlay--call').hide();
+    });
+});
+
+
+$(function ($) {
+    $('.navbar-burger--open').on('click', function () {
+        $(this).parents('.header-nav').toggleClass('header-nav--open');
+    });
+
+    $('html').on('click', function (e) {
+        if (!$(e.target).closest('.header-nav__burger').length &&
+            ($('.header-nav__burger').hasClass('header-nav--open'))) {
+            $('.header-nav__burger').removeClass('header-nav--open');
+        }
     });
 });
